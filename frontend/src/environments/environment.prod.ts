a641348259da574308c5953f5f0a198f477db6e6
Ravi <itsravit@example.com>
2025-09-18 23:56:00 +0530

    Modified Dockerfile, signup component html environment prod ts

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,13 @@
-// frontend/src/environments/environment.prod.ts
+// environment.prod.ts
+declare const API_URL: string;
+
+export const environment = {
+  production: true,
+  apiUrl: API_URL
+};
+
+/* // frontend/src/environments/environment.prod.ts
 export const environment = {
   production: true,
   apiUrl: '' // This will be replaced at build time
-};
+}; */
